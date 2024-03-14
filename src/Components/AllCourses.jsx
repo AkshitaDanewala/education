e6455849bce16f7b255d6fef3bf822249889e51b
AkshitaDanewala <akshitadanewala@gmail.com>
2024-03-14 15:37:35 +0530

    complete all couses page

--- src/Components/AllCourses.jsx
+++ src/Components/AllCourses.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const AllCourses = () => {
   return (
  <>
- 
-<div className="maindiv bg-[rgb(240,251,255)] w-full h-[163vh] pt-16 flex flex-col items-center pb-3">
+<div className="maindiv w-full h-[163vh] bg-[rgb(240,251,255)] pt-16 flex flex-col items-center pb-3">
 
 <div className="headingdiv py-3 w-full ">
 
@@ -13,7 +12,7 @@ const AllCourses = () => {
 
 <div className='h-[135vh] w-full  mt-3 py-2 px-3 flex justify-around'>
 
-<div className="courses h-[130vh] w-[50vw] flex flex-col items-center justify-between rounded-lg bg-white ">
+<div className="courses h-[130vh] w-[50vw] flex flex-col items-center justify-between rounded-lg bg-[rgb(255,255,255)] ">
 
 
 <div className="headingdiv py-3 w-full  flex items-center justify-around border-b-2 border-black">
@@ -22,17 +21,17 @@ const AllCourses = () => {
 <h1 className='text-[2.3vmax] font-semibold font-poppins'>Duration</h1>
 </div>
 
-{/* <div className='h-10 w-full  flex items-center justify-around border-b-2 border-black'>
+<div className='h-10 w-full  flex items-center justify-around border-b-2 border-black'>
   <div className="coursename h-10 w-[17vw] ">
     <h1 className='text-[1.4vmax] font-semibold'>GDA Nursing Assistant</h1>
   </div>
   <div className="durationdiv h-10 w-[10vw] px-2 ">
     <h1 className='text-[1.4vmax] font-semibold'>2 Years</h1>
   </div>
-</div> */}
+</div>
 
 
-{/* <div className='h-10 w-full  flex items-center justify-around border-b-2 border-black'>
+<div className='h-10 w-full  flex items-center justify-around border-b-2 border-black'>
   <div className="coursename h-10 w-[17vw] ">
   <h1 className='text-[1.4vmax] font-semibold'>Radiographer </h1>
 
@@ -42,9 +41,9 @@ const AllCourses = () => {
   <h1 className='text-[1.4vmax] font-semibold'>2 Years</h1>
 
   </div>
-</div> */}
+</div>
 
-{/* <div className='h-10 w-full  flex items-center justify-around border-b-2 border-black'>
+<div className='h-10 w-full  flex items-center justify-around border-b-2 border-black'>
   <div className="coursename h-10 w-[17vw]">
   <h1 className='text-[1.4vmax] font-semibold'>CSSD </h1>
 
@@ -53,9 +52,9 @@ const AllCourses = () => {
   <h1 className='text-[1.4vmax] font-semibold'>1 Year </h1>
 
   </div>
-</div> */}
+</div>
 
-{/* <div className='h-10 w-full  flex items-center justify-around border-b-2 border-black'>
+<div className='h-10 w-full  flex items-center justify-around border-b-2 border-black'>
   <div className="coursename h-10 w-[17vw]">
   <h1 className='text-[1.4vmax] font-semibold'>Dresser</h1>
 
@@ -64,9 +63,9 @@ const AllCourses = () => {
   <h1 className='text-[1.4vmax] font-semibold'>1 Year </h1>
 
   </div>
-</div> */}
+</div>
 
-{/* <div className='h-10 w-full  flex items-center justify-around border-b-2 border-black'>
+<div className='h-10 w-full  flex items-center justify-around border-b-2 border-black'>
   <div className="coursename h-10 w-[17vw]">
   <h1 className='text-[1.4vmax] font-semibold'>Lab Technician</h1>
 
@@ -75,9 +74,9 @@ const AllCourses = () => {
   <h1 className='text-[1.4vmax] font-semibold'>2 Years/3Years</h1>
 
   </div>
-</div> */}
+</div>
 
-{/* <div className='h-10 w-full  flex items-center justify-around border-b-2 border-black'>
+<div className='h-10 w-full  flex items-center justify-around border-b-2 border-black'>
   <div className="coursename h-10 w-[17vw]">
   <h1 className='text-[1.4vmax] font-semibold'>Medical Records Assistant</h1>
 
@@ -86,9 +85,9 @@ const AllCourses = () => {
   <h1 className='text-[1.4vmax] font-semibold'>2 Years</h1>
 
   </div>
-</div> */}
+</div>
 
-{/* <div className='h-10 w-full  flex items-center justify-around border-b-2 border-black'>
+<div className='h-10 w-full  flex items-center justify-around border-b-2 border-black'>
   <div className="coursename h-10 w-[17vw] ">
   <h1 className='text-[1.4vmax] font-semibold'>OT/ICU Technician</h1>
 
@@ -97,9 +96,9 @@ const AllCourses = () => {
   <h1 className='text-[1.4vmax] font-semibold'>2 Years</h1>
 
   </div>
-</div> */}
+</div>
 
-{/* <div className='h-10 w-full  flex items-center justify-around border-b-2 border-black'>
+<div className='h-10 w-full  flex items-center justify-around border-b-2 border-black'>
   <div className="coursename h-10 w-[17vw] ">
   <h1 className='text-[1.4vmax] font-semibold'>Diploma in Physiotherapy</h1>
 
@@ -108,9 +107,9 @@ const AllCourses = () => {
   <h1 className='text-[1.4vmax] font-semibold'>2 Years</h1>
 
   </div>
-</div> */}
+</div>
 
-{/* <div className='h-10 w-full  flex items-center justify-around border-b-2 border-black'>
+<div className='h-10 w-full  flex items-center justify-around border-b-2 border-black'>
   <div className="coursename h-10 w-[17vw] ">
   <h1 className='text-[1.4vmax] font-semibold'>Cardiac Care</h1>
   </div>
@@ -118,9 +117,9 @@ const AllCourses = () => {
   <div className="durationdiv h-10 w-[10vw] ">
   <h1 className='text-[1.4vmax] font-semibold'>1 Year</h1>
   </div>
-</div> */}
+</div>
 
-{/* <div className='h-10 w-full  flex items-center justify-around border-b-2 border-black'>
+<div className='h-10 w-full  flex items-center justify-around border-b-2 border-black'>
   <div className="coursename h-10 w-[17vw] ">
   <h1 className='text-[1.4vmax] font-semibold'>Dental Hygienist</h1>
   </div>
@@ -128,9 +127,9 @@ const AllCourses = () => {
   <div className="durationdiv h-10 w-[10vw] ">
   <h1 className='text-[1.4vmax] font-semibold'>1 Year</h1>
   </div>
-</div> */}
+</div>
 
-{/* <div className='h-10 w-full  flex items-center justify-around border-b-2 border-black'>
+<div className='h-10 w-full  flex items-center justify-around border-b-2 border-black'>
   <div className="coursename h-10 w-[17vw] ">
   <h1 className='text-[1.4vmax] font-semibold'>Sanitory Inspector</h1>
   </div>
@@ -138,10 +137,10 @@ const AllCourses = () => {
   <div className="durationdiv h-10 w-[10vw] ">
   <h1 className='text-[1.4vmax] font-semibold'>1 Year</h1>
   </div>
-</div> */}
+</div>
 
 
-{/* <div className='h-10 w-full flex items-center justify-around '>
+<div className='h-10 w-full flex items-center justify-around '>
   <div className="coursename h-10 w-[17vw] ">
   <h1 className='text-[1.4vmax] font-semibold'>First Aid</h1>
   </div>
@@ -149,71 +148,7 @@ const AllCourses = () => {
   <div className="durationdiv h-10 w-[10vw] ">
   <h1 className='text-[1.4vmax] font-semibold'>1 Year</h1>
   </div>
-</div> */}
-
-<table className='w-full border-collapse bg-red-200 '>
-  <tr>
-    <th className='p-[10px] text-center border-b-2 border-black'>Row 1</th>
-    <td className='p-[10px] text-center border-b-2 border-black'>Cell 1</td>
-  </tr>
-
-  <tr>
-    <th className='p-[10px] text-center border-b-2 border-black'>Row 2</th>
-    <td className='p-[10px] text-center border-b-2 border-black'>Cell 2</td>
-  </tr>
-
-  <tr>
-    <th className='p-[10px] text-center border-b-2 border-black'>Row 3</th>
-    <td className='p-[10px] text-center border-b-2 border-black'>Cell 3</td>
-  </tr>
-
-  <tr>
-    <th className='p-[10px] text-center border-b-2 border-black'>Row 4</th>
-    <td className='p-[10px] text-center border-b-2 border-black'>Cell 4</td>
-  </tr>
-
-  <tr>
-    <th className='p-[10px] text-center border-b-2 border-black'>Row 5</th>
-    <td className='p-[10px] text-center border-b-2 border-black'>Cell 5</td>
-  </tr>
-
-  <tr>
-    <th className='p-[10px] text-center border-b-2 border-black'>Row 6</th>
-    <td className='p-[10px] text-center border-b-2 border-black'>Cell 6</td>
-  </tr>
-
-  <tr>
-    <th className='p-[10px] text-center border-b-2 border-black'>Row 7</th>
-    <td className='p-[10px] text-center border-b-2 border-black'>Cell 7</td>
-  </tr>
-
-  <tr>
-    <th className='p-[10px] text-center border-b-2 border-black'>Row 8</th>
-    <td className='p-[10px] text-center border-b-2 border-black'>Cell 8</td>
-  </tr>
-
-  <tr>
-    <th className='p-[10px] text-center border-b-2 border-black'>Row 9</th>
-    <td className='p-[10px] text-center border-b-2 border-black'>Cell 9</td>
-  </tr>
-
-  <tr>
-    <th className='p-[10px] text-center border-b-2 border-black'>Row 10</th>
-    <td className='p-[10px] text-center border-b-2 border-black'>Cell 10</td>
-  </tr>
-
-  <tr>
-    <th className='p-[10px] text-center border-b-2 border-black'>Row 11</th>
-    <td className='p-[10px] text-center border-b-2 border-black'>Cell 11</td>
-  </tr>
-
-  <tr>
-    <th className='p-[10px] text-center border-b-2 border-black'>Row 12</th>
-    <td className='p-[10px] text-center border-b-2 border-black'>Cell 12</td>
-  </tr>
-</table>
-
-
+</div>
 
 
 
